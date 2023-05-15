081fcdf06400fbdcc9d78ccec0c3498a81ef171a
senyc <kdbomhof@gmail.com>
2023-05-15 16:48:00 -0400

    feat: changing go/O commands to move cursor as well
    Also some restructuring of remap.lua

--- .config/nvim/lua/senyc/remap.lua
+++ .config/nvim/lua/senyc/remap.lua
@@ -3,24 +3,24 @@ local map = vim.keymap.set
 
 map('i','jj', '<Esc>', options)
 -- Buffer deletion and traversal
-map('n', '<leader>l', ':bnext<CR>', options)
-map('n', '<leader>h', ':bprevious<CR>', options)
-map('n', '<leader>dd', ':w<CR> :NvimTreeClose <cr> :bdelete<cr> :LspRestart<cr>', options)
-map('n', '<leader>d!', ':NvimTreeClose <cr> :bdelete<cr> :LspRestart<cr>', options)
--- Centralized navigation
+map('n', '<leader>l', ':bnext<cr>', options)
+map('n', '<leader>h', ':bprevious<cr>', options)
+map('n', '<leader>dd', [[:w<cr> :NvimTreeClose <cr> :bdelete<cr> :LspRestart<cr>]], options)
+map('n', '<leader>d!', [[:NvimTreeClose<cr> :bdelete<cr> :LspRestart<cr>]], options)
+-- Centralized navigation for search and <c-d/u>
 map('n', '<c-d>', '<c-d>zz', options)
 map('n', '<c-u>', '<c-u>zz', options)
 map('n', 'n', 'nzzzv')
 map('n', 'N', 'Nzzzv')
 -- Allow for easy empty line adjustments
-map('n', 'go', [[:set paste<CR>m`o<Esc>``:set nopaste<CR>]],{noremap = true} )
-map('n', 'gO', [[:set paste<CR>m`O<Esc>``:set nopaste<CR>]], options)
+map('n', 'go', [[o<Esc>]], options )
+map('n', 'gO', [[O<Esc>]], options)
 map('n', 'Y', 'y$', options)
 -- Allow for vis mode to move lines
-map('v', 'J', ":m '>+1<CR>gv=gv", options)
+map('v', 'J', ":m '>+1<cr>gv=gv", options)
 map('v', 'K', ":m '<-2<CR>gv=gv", options)
--- Adjustment of cursor location in J and inverse addition
-map('n', '<C-j>', 'a<cr><Esc>13', options)
+-- Adjustment of cursor location in J and inverse addition for <c-j>
+map('n', '<C-j>', 'a<cr><Esc>', options)
 map('n', 'J', 'mzJ`z', options)
 -- System clipboard adjustments
 map({'n','v'}, '<leader>Y', [["+Y]], options)
@@ -36,3 +36,4 @@ map('n', '<leader>rw', [[m`:%s/\s\+$//e<cr>``]], options)
 map('n', '<leader>wq', ":wa <cr> :qa!<cr>")
 -- Remap to highlight all of the file
 map('n', '<C-a>', 'GVgg', options)
+
