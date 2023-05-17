9ca09f2a0d9aa14ba63decf0d5f46926331f7faf
senyc <kdbomhof@gmail.com>
2023-05-17 18:21:03 -0400

    feat: adding full dir on lualine

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -5,7 +5,7 @@ require('lualine').setup {
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
-       'packer', 'NvimTree', 'undotree'
+      'packer', 'NvimTree', 'undotree'
     },
     ignore_focus = {},
     always_divide_middle = true,
@@ -19,7 +19,11 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {{
+      'filename',
+      file_status = true,
+      path = 2
+    }},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
