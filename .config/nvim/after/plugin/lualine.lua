979b110b0f198537fd4524040664c9448c6245c8
senyc <95313103+senyc@users.noreply.github.com>
2023-11-13 10:59:06 -0500

    fix(nvim): remove lualine on splash screen

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -5,7 +5,7 @@ require('lualine').setup {
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = {
-      'packer', 'NvimTree', 'undotree'
+      'packer', 'NvimTree', 'undotree', 'alpha'
     },
     ignore_focus = {},
     always_divide_middle = true,
